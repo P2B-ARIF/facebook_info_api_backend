5b498e8a4693338c34a3b26459e3e42e1fc4b44e
MD <p2b.business.info@gmail.com>
2024-10-13 20:50:27 +0600

    chnage everyitng

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const rateLimit = require("express-rate-limit");
 const {
 	generatePhoneNumber,
 	getTempEmail,
@@ -10,21 +12,20 @@ const {
 	getGirlsNames,
 } = require("./utils/factory");
 const { connectToDatabase } = require("./utils/db");
-const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
-const bcrypt = require("bcrypt");
-const rateLimit = require("express-rate-limit");
 const { startCleanupJob } = require("./utils/guarduser");
 
+dotenv.config(); // Load environment variables
+
+const app = express();
 const PORT = process.env.PORT || 5001;
 const JWT_SECRET = process.env.JWT_SECRET;
 const saltRounds = 10;
 
-dotenv.config();
-
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Rate limiter (60 requests per minute)
 const limiter = rateLimit({
 	windowMs: 60 * 1000,
 	max: 60,
@@ -35,11 +36,12 @@ const limiter = rateLimit({
 	standardHeaders: true,
 	legacyHeaders: false,
 });
+
 app.use(limiter);
 
-// Middleware to authenticate the JWT
+// JWT Authentication Middleware
 const authenticateToken = (req, res, next) => {
-	const token = req.headers.authorization?.split(" ")[1]; // Expecting format: "Bearer TOKEN"
+	const token = req.headers.authorization?.split(" ")[1];
 	if (!token) {
 		return res
 			.status(401)
@@ -47,8 +49,8 @@ const authenticateToken = (req, res, next) => {
 	}
 	try {
 		const decoded = jwt.verify(token, JWT_SECRET);
-		req.user = decoded; // Add the decoded token data (userId, email) to the request object
-		next(); // Continue to the next middleware or route handler
+		req.user = decoded;
+		next();
 	} catch (error) {
 		return res
 			.status(403)
@@ -59,85 +61,75 @@ const authenticateToken = (req, res, next) => {
 // Start the cleanup job when the server starts
 startCleanupJob();
 
-// LET'S START
+// Public Route
 app.get("/", (req, res) => {
 	res.json({ response: "This is a public route accessible to everyone." });
 });
 
-// USER VERIFY
+// Verify User Route
 app.get("/user_verify", authenticateToken, async (req, res) => {
-	try {
-		res.status(200).json({ access: true });
-	} catch (err) {
-		res.status(500).json({ access: false, message: err.message });
-	}
+	res.status(200).json({ access: true });
 });
 
-// GET 2FA CODE
+// Get 2FA Code Route
 app.get("/get_2fa_code", authenticateToken, async (req, res) => {
 	try {
 		const { key } = req.query;
 		const code = await get2FACode(key);
 		res.json(code);
 	} catch (err) {
-		res
-			.status(404)
-			.json({ access: false, message: "Something went wrong...!" });
+		res.status(500).json({ access: false, message: "Something went wrong!" });
 	}
 });
 
-// CHECK INBOX
+// Check Inbox Route
 app.get("/check_inbox", authenticateToken, async (req, res) => {
 	try {
 		const { email } = req.query;
 		const inbox = await fetchInbox(email);
-		if (inbox) {
-			res.json(inbox);
-		} else {
-			res.status(404).json({ access: true, message: "No emails found." });
+		if (!inbox || inbox.length === 0) {
+			return res
+				.status(404)
+				.json({ access: true, message: "No emails found." });
 		}
+		res.json(inbox);
 	} catch (err) {
-		res
-			.status(404)
-			.json({ access: false, message: "Something went wrong...!" });
+		res.status(500).json({ access: false, message: "Something went wrong!" });
 	}
 });
 
-// GET FACEBOOK INFO
+// Get Random Details Route
 app.get("/get_details", authenticateToken, async (req, res) => {
 	try {
 		const randomGirlName = await getGirlsNames();
 		const randomNumber = await generatePhoneNumber();
 		const tempEmail = await getTempEmail();
 
-		return res.status(200).json({
+		res.status(200).json({
 			girlName: randomGirlName,
 			number: randomNumber,
 			email: tempEmail,
 		});
 	} catch (err) {
-		res.status(404).json("Something went wrong...!");
+		res.status(500).json({ message: "Something went wrong!" });
 	}
 });
 
-// PASSWORD BCRYPT
+// Password Hashing Route
 app.get("/password/bcrypt", async (req, res) => {
 	try {
 		const { password } = req.query;
 		if (!password) {
 			return res.status(400).json({ message: "Password query is required" });
 		}
-		const salt = await bcrypt.genSalt(saltRounds);
-		const hashedPassword = await bcrypt.hash(password, salt);
-
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
 		res.status(200).json({ hashedPassword });
 	} catch (error) {
-		console.error("Error generating bcrypt password:", error);
 		res.status(500).json({ message: "Server error" });
 	}
 });
 
-// USER LOGIN
+// User Login Route
 app.put("/auth/login", async (req, res) => {
 	try {
 		const { email, password } = req.body;
@@ -154,58 +146,51 @@ app.put("/auth/login", async (req, res) => {
 			return res.status(401).json({ message: "Invalid credentials" });
 		}
 		if (!user.access) {
-			return res.status(403).json({
-				access: false,
-				message:
-					"Access denied: same email login on multiple devices not allowed",
-			});
+			return res
+				.status(403)
+				.json({
+					access: false,
+					message:
+						"Access denied: same email login on multiple devices not allowed",
+				});
 		}
 
 		const token = jwt.sign(
 			{ userId: user._id, email: user.email },
-			process.env.JWT_SECRET,
+			JWT_SECRET,
 			{ expiresIn: "30d" },
 		);
 
 		await usersCollection.updateOne({ email }, { $set: { access: false } });
 		res.status(200).json({ access: true, message: "Login successful", token });
 	} catch (error) {
-		// Log error and respond with generic error message
-		console.error("Error during login:", error);
 		res.status(500).json({ message: "Server error" });
 	}
 });
 
-// USER BLOCK
+// Block User Route
 app.put("/block-user", async (req, res) => {
 	try {
 		const { email } = req.body;
 		if (!email) {
-			return res.status(400).json({ message: "Email are required" });
+			return res.status(400).json({ message: "Email is required" });
 		}
 		const db = await connectToDatabase();
 		const usersCollection = db.collection("users");
 
-		const exists = await usersCollection.findOne({ email });
-		if (!exists)
-			return res.status(500).json({ message: "This users already exists" });
+		const user = await usersCollection.findOne({ email });
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
 
-		const result = await usersCollection.updateOne(
-			{ email },
-			{ $set: { access: false } },
-			{ unset: true },
-		);
-		res.status(201).json({
-			message: "User updated successfully",
-			userId: result.insertedId,
-		});
+		await usersCollection.updateOne({ email }, { $set: { access: false } });
+		res.status(200).json({ message: "User blocked successfully" });
 	} catch (error) {
-		console.error("Error adding user:", error);
 		res.status(500).json({ message: "Server error" });
 	}
 });
 
-// ADD USER
+// Add User Route
 app.post("/add-user", async (req, res) => {
 	try {
 		const { email, password, name } = req.body;
@@ -218,8 +203,12 @@ app.post("/add-user", async (req, res) => {
 		const db = await connectToDatabase();
 		const usersCollection = db.collection("users");
 
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
+		const existingUser = await usersCollection.findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
 
 		const newUser = {
 			name,
@@ -228,17 +217,12 @@ app.post("/add-user", async (req, res) => {
 			createdAt: new Date(),
 			access: true,
 		};
-		const exists = await usersCollection.findOne({ email });
-		if (exists)
-			return res.status(500).json({ message: "This users already exists" });
 
 		const result = await usersCollection.insertOne(newUser);
-		res.status(201).json({
-			message: "User added successfully",
-			userId: result.insertedId,
-		});
+		res
+			.status(201)
+			.json({ message: "User added successfully", userId: result.insertedId });
 	} catch (error) {
-		console.error("Error adding user:", error);
 		res.status(500).json({ message: "Server error" });
 	}
 });
